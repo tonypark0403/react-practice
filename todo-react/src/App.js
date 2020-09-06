87c5eceee1451ac030087ed2252cc9471b83df56
Tony Park <tonypark0403@gmail.com>
2020-09-06 15:36:09 -0400

    compared code between js and react

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -2,15 +2,48 @@ import React, { useState } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  function onAdd() {}
-  function onDelete() {}
+  const [currentId, setCurrentId] = useState(1);
+  const [desc, setDesc] = useState('');
+  const [showOdd, setShowOdd] = useState(false);
+
+  function onAdd() {
+    const todo = {
+      id: currentId,
+      desc,
+    };
+    setCurrentId(currentId + 1);
+    setTodoList([...todoList, todo]);
+  }
+  function onDelete(e) {
+    const id = Number(e.target.dataset.id);
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(newTodoList);
+  }
   function onSaveToServer() {}
   return (
     <div>
       <h3>Todo List</h3>
-      <ul></ul>
-      <input type='text' value={''} onChange={(e) => {}} />
+      <ul>
+        {todoList
+          .filter((_, index) => (showOdd ? index % 2 === 0 : true))
+          .map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.desc}</span>
+              <button data-id={todo.id} onClick={onDelete}>
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+      <input
+        type='text'
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
       <button onClick={onAdd}>Add</button>
+      <button onClick={() => setShowOdd(!showOdd)}>
+        only show for odd items on / off
+      </button>
       <button onClick={onSaveToServer}>Save</button>
     </div>
   );
