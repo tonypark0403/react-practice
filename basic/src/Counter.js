a1a1c1dd64d9e79ca9d0d8b30e4b399723fb433d
Tony Park <tonypark0403@gmail.com>
2020-09-09 22:31:21 -0400

    tested count

--- basic/src/Counter.js
+++ basic/src/Counter.js
@@ -2,19 +2,16 @@ import React, { useState } from 'react';
 import Title from './Title';
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const [count, setCount] = useState({ value: 0 });
   function onClick() {
-    setCount(count + 1);
-  }
-  function onClick2() {
-    setCount(count2 + 1);
+    // count.value += 1;
+    // setCount(count);
+    setCount({ ...count, value: count.value + 1 });
   }
   return (
     <div>
-      <Title title={`Current count is ${count}`} />
+      <Title title={`Current count is ${count.value}`} />
       <button onClick={onClick}>Increase</button>
-      <button onClick={onClick2}>Increase2</button>
     </div>
   );
 }
