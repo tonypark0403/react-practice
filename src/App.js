0bc587c6c74e01eb9e1eba95c42e22cdad347898
Tony Park <tonypark0403@gmail.com>
2020-09-15 12:21:44 -0400

    designed header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from './search/container/Search';
+import 'antd/dist/antd.css';
 
 export default function App() {
   return <Search />;
--- src/search/container/Search.js
+++ src/search/container/Search.js
@@ -1,5 +1,25 @@
+import { Col, Row, Typography } from 'antd';
 import React from 'react';
+import Settings from '../component/Settings';
 
 export default function Search() {
-  return <div>Search</div>;
+  return (
+    <>
+      <Row justify='end' style={{ padding: 20 }}>
+        <Col>
+          <Settings />
+        </Col>
+      </Row>
+      <Row justify='center' style={{ marginTop: 100 }}>
+        <Col>
+          <Typography.Title style={{ fontFamily: 'Caligrahhy' }}>
+            Management
+          </Typography.Title>
+        </Col>
+      </Row>
+      <Row justify='center' style={{ marginTop: 50 }}>
+        <Col span={12}>Search</Col>
+      </Row>
+    </>
+  );
 }
